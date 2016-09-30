c62b8e7960259c669d732c1eaf8b1b3e6c33e2fc
Davis Engeler <davisengeler+github@gmail.com>
2016-09-30 01:10:34 -0400

    Add mouseover feedback and corrected some logic

--- tracklist.php
+++ tracklist.php
@@ -17,24 +17,32 @@
 <script type="text/javascript">
 $(document).ready(function(){
 	var lastPreview = nav_audio;
+	var lastPreviewPlaying = false;
 	var lastSongID = -1;
     $(".song").click(
     	function(){
 	    	var audio = "#" + $(this).attr("id") + "_preview";
 	    	var preview = $(audio)[0];
-	    	if (preview == lastPreview) 
+	    	var thisSongID = $(this).attr("id");
+	    	if ((lastSongID == thisSongID) || (lastSongID == -1)) {
 	    		if (preview.paused) {
 	    			preview.play();
 	    			$("i.preview-icon-" + $(this).attr("id")).removeClass("fa-play").addClass("fa-pause");
+	    			lastPreviewPlaying = true;
 	    		}
 	    		else {
 	    			preview.pause();
-	    			$("i.preview-icon-" + $(this).attr("id")).removeClass("fa-pause").addClass("fa-music");
+	    			$("i.preview-icon-" + $(this).attr("id")).removeClass("fa-pause").addClass("fa-play");
+	    			lastPreviewPlaying = false;
 	    		}
+	    	}
 	    	else {
-	    		$("i.preview-icon-" + lastSongID).removeClass("fa-pause").addClass("fa-music");
+	    		if (!lastPreview.paused) {
+		    		$("i.preview-icon-" + lastSongID).removeClass("fa-pause").addClass("fa-music");
+		    		lastPreview.pause();
+		    		lastPreviewPlaying = true;
+		    	}
 	    		$("i.preview-icon-" + $(this).attr("id")).removeClass("fa-play").addClass("fa-pause");
-	    		lastPreview.pause();
 	    		preview.play();
 	    	}
 	    	lastPreview = preview;
@@ -42,8 +50,11 @@ $(document).ready(function(){
     	});
     $(".song").hover(
     	function(){
-    		$(this).find('.love').toggleClass('hoverClass');
-    		// $("i.preview-icon-" + $(this).attr("id")).toggleClass('fa-music fa-play')
+    		$(this).parent().find('.love').toggleClass('hoverClass');
+    		// TODO: if has attribute fa-music or fa-play
+    		if ($("i.preview-icon-" + $(this).attr("id")).hasClass("fa-music") || $("i.preview-icon-" + $(this).attr("id")).hasClass("fa-play")) {
+    			$("i.preview-icon-" + $(this).attr("id")).toggleClass('fa-music fa-play')
+    		}
     	})
 });
 </script>
@@ -84,15 +95,14 @@ $(document).ready(function(){
                     $youtube = isset($song['youtube']) ? $song['youtube'] : false;
                     $download = isset($song['download']) ? $song['download'] : false;
                     $fullName = $song['artist'] . " - " .$song['name'];
-                    $fileLocation = get_template_directory_uri() . "/" . $fullName;
+                    $fileLocation = "../song-previews/" . $fullName;
                     echo '
-                        <tr id="' . $song['songID'] . '" class="song">
+                        <tr>
                         	<audio loop id="' . $song['songID'] . '_preview" preload="none">
 							    <source src="' . $fileLocation . '.mp3" type="audio/mpeg"></source>
-							    <source src="' . $fileLocation . '.ogg" type="audio/ogg"></source>
 							</audio>
-							<td><i class="preview-icon-' . $song['songID'] . ' fa fa-music" aria-hidden="true"></i></td>
-							<td><b>' . $song['name'] . '</b></td>
+							<td id="' . $song['songID'] . '" class="song"><i class="preview-icon-' . $song['songID'] . ' fa fa-music" aria-hidden="true"></i></td>
+							<td id="' . $song['songID'] . '" class="song"><b>' . $song['name'] . '</b></td>
 							<td><a target="blank" href="' . $song['artistLink'] . '">' . $song['artist'] . '</a></td>
 							<td>';
 							    if ($spotify) 
